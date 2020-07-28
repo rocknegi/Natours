1a080ef825ba7d548b1ca99a942d3ac1d1618c77
Rohit Singh Negi <rocknegi53@gmail.com>
2020-07-28 12:22:21 +0530

    add a logo and a header

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,16 @@ import './App.css'
 
 export default function App() {
     return (
-        <div>
-            GG
-        </div>
+        <header className='header'>
+            <div className='logo-box'>
+                <img src={require('../assets/img/logo-white.png')} alt='Logo' className='logo' />
+            </div>
+            <div className='text-box'>
+                <h1 className='heading-primary'>
+                    <span className='heading-primary-main'>Outdoors</span>
+                    <span className='heading-primary-sub'>is where life happens</span>
+                </h1>
+            </div>
+        </header>
     )
 }
